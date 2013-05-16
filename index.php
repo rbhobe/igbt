21ae0acf4bcd781d67ec75975d278a783d457790
Rohan Bhobe <rohan.bhobe@gmail.com>
2013-05-16 00:41:22 -0700

    setting up 2 column product page structure

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Skunkworks</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1>Stealth</h1>
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span2">
+          <!--Sidebar content-->
+          
+        </div>
+        <div class="span10">
+          <!--Body content-->
+          
+        </div>
+      </div>
+    </div>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+
+<?php
+
+?>
